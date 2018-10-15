b262cd20e5fbd01368545a2c79bcb1c262e42f16
Klopsch <Max.Klopsch@thomsonreuters.com>
2018-10-15 19:30:16 +0100

    Add flash cards and cards template and routes

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/flashcards', (req, res) => {
+  res.render('flashcards');
+});
+
+app.get('/cards', (req, res) => {
+//  res.locals.prompt = "Who is buried in Grant's tomb?";
+  res.locals = {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."};
+  res.render('card');
+//  res.render('card', {prompt: "Who is buried in Grant's tomb?"});
+});
+
 app.get('/test', (req, res) => {
   res.status(200).send('<h1>This is the test page!</h1>').end();
 });
