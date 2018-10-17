fafc1f504c5c834dc36511c42d68028591514a24
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-10-17 14:13:42 +0100

    Adding links to home page

--- routes/cards.js
+++ routes/cards.js
@@ -14,24 +14,24 @@ router.get('/:id', (req, res) => {
 
     if(!side) {
         res.redirect(`/cards/${id}?side=question`);
+    } else {
+        const text = cards[id][side];
+        const {hint} = cards[id];
+        const templateData = {id, text};
+
+        if(side === 'question') {
+            templateData.hint = hint;
+            templateData.sideToShow = 'answer';
+            templateData.sideToShowDisplay = 'Answer';
+        } else if(side === 'answer') {
+            templateData.sideToShow = 'question';
+            templateData.sideToShowDisplay = 'Question';
+        }
+
+        //  res.locals.prompt = "Who is buried in Grant's tomb?";
+        res.render('card', templateData);
+        //  res.render('card', {prompt: "Who is buried in Grant's tomb?"});
     }
-
-    const text = cards[id][side];
-    const {hint} = cards[id];
-    const templateData = {id, text};
-
-    if(side === 'question') {
-        templateData.hint = hint;
-        templateData.sideToShow = 'answer';
-        templateData.sideToShowDisplay = 'Answer';
-    } else if(side === 'answer') {
-        templateData.sideToShow = 'question';
-        templateData.sideToShowDisplay = 'Question';
-    }
-
-    //  res.locals.prompt = "Who is buried in Grant's tomb?";
-    res.render('card', templateData);
-    //  res.render('card', {prompt: "Who is buried in Grant's tomb?"});
 });
 
 module.exports = router;
