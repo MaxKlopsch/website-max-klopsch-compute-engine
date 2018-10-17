62bba9fa0b557967bdd7301ba26326ac2a3296b7
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-10-17 13:36:55 +0100

    Updating cards to use query string for side

--- routes/cards.js
+++ routes/cards.js
@@ -4,21 +4,14 @@ const {data} = require('../data/flashcardData.json');
 const {cards} = data;
 
 router.get('/:id', (req, res) => {
-    const colors = [
-        'red',
-        'orange',
-        'yellow',
-        'green',
-        'blue',
-        'purple'
-    ];
+    const {side} = req.query;
+    const {id} = req.params;
+    const text = cards[id][side];
+    const {hint} = cards[id];
+    const templateData = {text, hint, side};
+
     //  res.locals.prompt = "Who is buried in Grant's tomb?";
-    res.locals = {
-        prompt: cards[req.params.id].question,
-        hint: cards[req.params.id].hint,
-        colors: colors
-    };
-    res.render('card');
+    res.render('card', templateData);
     //  res.render('card', {prompt: "Who is buried in Grant's tomb?"});
 });
 
