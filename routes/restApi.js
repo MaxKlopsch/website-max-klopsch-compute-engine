173f608a0332c6e017612c072d56c256afc900ce
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-10-19 12:55:33 +0100

    Create GET and POST route for questions

--- routes/restApi.js
+++ routes/restApi.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// GET /questions
+// Return question collection
+router.get("/", (req, res) => {
+    res.json({response: "Received a GET request"});
+});
+
+// POST /questions
+// Create a question
+router.post("/", (req, res) => {
+    res.json({
+        response: "Received a POST request",
+        body: req.body
+    });
+});
+
 module.exports = router;
