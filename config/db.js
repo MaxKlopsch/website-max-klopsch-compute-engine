d4e4d70741c90611e0bd7f4c3269d4155ba59986
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-10-23 16:13:54 +0100

    Cleanup to start work on Q&A API

--- config/db.js
+++ config/db.js
@@ -19,43 +19,4 @@ db.on("error", (err) => {
 db.once("open", () => {
     console.log("DB connection successful");
 
-    const Schema = mongoose.Schema;
-    const AnimalSchema = new Schema({
-        type: {type: String, default: "goldfish"},
-        size: {type: String, default: "small"},
-        color: {type: String, default: "golden"},
-        mass: {type: Number, default: 0.007},
-        name: {type: String, default: "Garry"}
-    });
-
-    const Animal = mongoose.model("Animal", AnimalSchema);
-
-    const elephant = new Animal({
-        type: "elephant",
-        size: "big",
-        color: "gray",
-        mass: 6000,
-        name: "Lawrence"
-    });
-
-    const animal = new Animal({}); // Goldfish
-
-    Animal.remove({}, () => {
-        elephant.save((err) => {
-            if(err) {
-                console.error("Save Failed.", err);
-            } else {
-                animal.save(() => {
-                    if(err) {
-                        console.error("Save Failed.", err);
-                    } else {
-                        db.close(() => {
-                            console.log("DB connection closed.");
-                        });
-                    }
-                });  
-            }  
-        });
-    });
-
 });
