5c805968e1f832ea1816770358a9911144275edd
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-10-24 14:23:33 +0100

    Impleting sorting of answers

--- config/models.js
+++ config/models.js
@@ -2,6 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const sortAnswers = function(a, b) {
+    // - negative a before b
+    // 0 no change
+    // + postive a after b
+    if(a.votes === b.votes) {
+        if(a.updatedAt > b.updatedAt) {
+            return -1;
+        } else if(a.updatedAt < b.updatedAt) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    return b.votes - a.votes;
+};
+
 const AnswerSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
@@ -13,6 +29,11 @@ const QuestionSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
     answers: [AnswerSchema]
+});
+
+QuestionSchema.pre("save", () => {
+    this.answers.sort();
+    next();
 });
 
 const Question = mongoose.model("Question", QuestionSchema);
