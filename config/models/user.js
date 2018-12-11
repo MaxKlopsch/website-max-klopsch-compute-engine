7c7d8b2fd3a4139772bd9fbfd4d468c486f91102
Klopsch <Max.Klopsch@thomsonreuters.com>
2018-12-11 15:58:56 +0000

    Hash password before saving to database

--- config/models/user.js
+++ config/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -21,6 +22,18 @@ const UserSchema = new mongoose.Schema({
       type: String,
       required: true
     }
+});
+
+// hash password before saving to database
+UserSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model('User', UserSchema);
