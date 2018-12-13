317dda043e1b3801e09c7c8f7a63970646ae2f9c
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-12-13 11:35:55 +0000

    Remove duplicate requiresLogin function from profile route

--- routes/bookApp.js
+++ routes/bookApp.js
@@ -97,11 +97,6 @@ router.post('/login', (req, res, next) => {
 
 // GET /profile
 router.get('/profile', mid.requiresLogin, function(req, res, next) {
-    if (! req.session.userId ) {
-      var err = new Error("You are not authorized to view this page.");
-      err.status = 403;
-      return next(err);
-    }
     User.findById(req.session.userId)
         .exec(function (error, user) {
           if (error) {
