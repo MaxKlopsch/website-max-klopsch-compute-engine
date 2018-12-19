41b087b432580808069aad8190a0c373fef9115c
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-12-19 14:50:46 +0000

    Adding function to change displayed media depending on selection

--- public/js/vue-app.js
+++ public/js/vue-app.js
@@ -54,11 +54,15 @@ const app = new Vue({
     el: '#media-list',
     data: {
         title: 'Public Book Library',
-        mediaList: media
+        mediaList: media,
+        type: ''
     },
     methods: {
         toggleDetails: function(media) {
             media.showDetail = !media.showDetail;
+        },
+        filterList: function() {
+            this.type = event.target.value;
         }
     }
 });
