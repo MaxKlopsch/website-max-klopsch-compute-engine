21d6bd578aba2d42de4ab0f824eabbfe87e08c16
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-12-31 13:35:17 +0000

    Add confirmed checkbox and update class when checkbox is checked on RSVP app

--- public/js/rsvp-app.js
+++ public/js/rsvp-app.js
@@ -6,10 +6,28 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = input.value;
     if (text) {
+        input.value = '';
+
         const li = document.createElement('li');
         li.textContent = text;
+        const label = document.createElement('label');
+        label.textContent = 'Confirmed';
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        label.appendChild(checkbox);
+        li.appendChild(label);
         ul.appendChild(li);
+    }
+});
 
-        input.value = '';
+ul.addEventListener('change', (e) => {
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+
+    if (checked) {
+        listItem.className = 'responded';
+    } else {
+        listItem.className = '';
     }
 });
