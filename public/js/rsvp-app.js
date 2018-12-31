b5f1effa0cb0def4644dbbdccd57875ffbee06b6
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2018-12-31 13:47:09 +0000

    Add remove function and move createLi to function in RSVP app

--- public/js/rsvp-app.js
+++ public/js/rsvp-app.js
@@ -2,20 +2,32 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
+function createLi(text) {
+    const li = document.createElement('li');
+    li.textContent = text;
+
+    // append 'Confirmed' label and checkbox
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    label.appendChild(checkbox);
+    li.appendChild(label);
+
+    // append 'Remove' button
+    const button = document.createElement('button');
+    button.textContent = 'Remove';
+    li.appendChild(button);
+
+    return li;
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = input.value;
     if (text) {
         input.value = '';
-
-        const li = document.createElement('li');
-        li.textContent = text;
-        const label = document.createElement('label');
-        label.textContent = 'Confirmed';
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        label.appendChild(checkbox);
-        li.appendChild(label);
+        const li = createLi(text);
         ul.appendChild(li);
     }
 });
@@ -29,5 +41,12 @@ ul.addEventListener('change', (e) => {
         listItem.className = 'responded';
     } else {
         listItem.className = '';
+    }
+});
+
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const li = e.target.parentNode;
+        ul.removeChild(li);
     }
 });
