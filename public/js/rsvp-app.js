df161d1bdddd5c15f4e3c6b30500cadb60c03f28
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-01-02 14:28:46 +0000

    Refactor list item click event handler in RSVP app

--- public/js/rsvp-app.js
+++ public/js/rsvp-app.js
@@ -84,24 +84,31 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target.tagName === 'BUTTON') {
             const button = e.target;
             const li = button.parentNode;
-            if (button.textContent === 'Remove') {
-                ul.removeChild(li);
-            } else if (button.textContent === 'Edit') {
-                const span = li.firstElementChild;
-                const input = document.createElement('input');
-                input.type = 'text';
-                input.value = span.textContent;
-                li.insertBefore(input, span);
-                li.removeChild(span);
-                button.textContent = 'Save';
-            } else if (button.textContent === 'Save') {
-                const input = li.firstElementChild;
-                const span = document.createElement('span');
-                span.textContent = input.value;
-                li.insertBefore(span, input);
-                li.removeChild(input);
-                button.textContent = 'Edit';
-            }
+
+            const nameActions = {
+                remove: () => {
+                    ul.removeChild(li);
+                },
+                edit: () => {
+                    const span = li.firstElementChild;
+                    const input = document.createElement('input');
+                    input.type = 'text';
+                    input.value = span.textContent;
+                    li.insertBefore(input, span);
+                    li.removeChild(span);
+                    button.textContent = 'Save';
+                },
+                save: () => {
+                    const input = li.firstElementChild;
+                    const span = document.createElement('span');
+                    span.textContent = input.value;
+                    li.insertBefore(span, input);
+                    li.removeChild(input);
+                    button.textContent = 'Edit';
+                }
+            };
+
+            nameActions[button.textContent.toLowerCase()]();
         }
     });
 });
