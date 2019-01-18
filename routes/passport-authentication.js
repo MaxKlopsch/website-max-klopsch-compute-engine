6ec5c584ac6037820b933a5358fd3f630c2a8249
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-01-18 16:35:04 +0000

    Make baseUrl available to routes and add to templates

--- routes/passport-authentication.js
+++ routes/passport-authentication.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// make baseUrl available in templates
+router.use((req, res, next) => {
+    res.locals.baseUrl = req.baseUrl;
+    next();
+});
+
 router.get('/', (req, res) => {
     res.render('passportAuthApp/welcome');
 });
