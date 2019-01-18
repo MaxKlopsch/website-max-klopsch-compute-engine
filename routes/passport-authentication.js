89a729d20e05a7f6310d1e160e7376dd5c1802fc
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-01-18 14:22:34 +0000

    Setting up pug layout and testing integration

--- routes/passport-authentication.js
+++ routes/passport-authentication.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("This works!");
+    res.render('passportAuthApp/welcome');
 });
 
 router.get('/login', (req, res) => {
