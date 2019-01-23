5dbfe65254dedf4dc9ba9fb90b48238c16f03740
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-01-23 13:11:46 +0000

    Set up Registration Page Handler

--- routes/passport-authentication.js
+++ routes/passport-authentication.js
@@ -7,16 +7,46 @@ router.use((req, res, next) => {
     next();
 });
 
+// Welcome Page
 router.get('/', (req, res) => {
     res.render('passportAuthApp/welcome');
 });
 
+// Login Page
 router.get('/login', (req, res) => {
     res.render('passportAuthApp/login');
 });
 
+// Registration Page
 router.get('/register', (req, res) => {
     res.render('passportAuthApp/register');
+});
+
+// Registration Handler
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({msg: 'Please fill in all required fields.'});
+    }
+
+    // Check passwords match
+    if (password !== password2) {
+        errors.push({msg: 'Passwords do not match.'});
+    }
+
+    // Check password length
+    if (password.length < 6) {
+        errors.push({msg: 'Password should be at least 6 characters long.'});
+    }
+
+    if (errors.length > 0) {
+        res.render('passportAuthApp/register', { errors, name, email, password, password2 });
+    } else {
+        res.send('Passed');
+    }
 });
 
 module.exports = router;
