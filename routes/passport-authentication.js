8c318fa1e7118431ee709c16bc14b3c375db1270
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-01-23 14:06:01 +0000

    Check for existing user and if not then save new user

--- routes/passport-authentication.js
+++ routes/passport-authentication.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// User model
+const User = require('../config/models/passport-user');
 
 // make baseUrl available in templates
 router.use((req, res, next) => {
@@ -43,9 +47,37 @@ router.post('/register', (req, res) => {
     }
 
     if (errors.length > 0) {
+        // Display errors and show registration page again
         res.render('passportAuthApp/register', { errors, name, email, password, password2 });
     } else {
-        res.send('Passed');
+        // Validation passed
+        User.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                // User exists
+                errors.push({msg: 'Email is already registered.'});
+                res.render('passportAuthApp/register', { errors, name, email, password, password2 });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                // Hash Password
+                bcrypt.hash(newUser.password, 10, (err, hash) => {
+                    if(err) throw err;
+                    // Set password to hashed
+                    newUser.password = hash;
+                    // Save user
+                    newUser.save()
+                        .then(user => {
+                            res.redirect(`${req.baseUrl}/login`);
+                        })
+                        .catch(err => console.error(err));
+                });
+            }
+        });
     }
 });
 
