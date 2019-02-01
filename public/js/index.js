c231a24d9ac9e9c01b73701849a2c31ef71c9193
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-02-01 15:26:41 +0000

    Remove unused JS code for accordion and add defer keyword

--- public/js/index.js
+++ public/js/index.js
@@ -8,21 +8,3 @@ function toggleMenu() {
         x.classList.add("hiddenMobile");
     }
 }
-
-const acc = document.getElementsByClassName("accordion");
-
-for (let i = 0; i < acc.length; i++) {
-    if (i === 0) {
-        acc[i].classList.toggle("active");
-        acc[i].nextElementSibling.style.maxHeight = acc[i].nextElementSibling.scrollHeight + "px";
-    }
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        const panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
