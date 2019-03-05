03917d21a9e1cd072ba56e3e438011c480ae6066
Klopsch <Max.Klopsch@thomsonreuters.com>
2019-03-05 14:01:22 +0000

    Create quote_rest_api.js route file and outline project

---
+++ routes/quote_rest_api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Send a GET request to /quotes to READ a list of quotes
+// Send a GET request to /quotes/:id to READ a quote
+// Send a GET request to /quotes/random to READ a random quote
+// Send a POST request to /quotes to CREATE a new quote
+// Send a PUT request to /quotes/:id to UPDATE a quote
+// Send a DELETE request to /quotes/:id to DELETE a quote
+
+module.exports = router;
