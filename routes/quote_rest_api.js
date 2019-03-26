66d5dadfad67be2cac394ed490e9d3b39c05428e
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-03-26 13:37:42 +0000

    Implement error catcher for internal error and 404

--- routes/quote_rest_api.js
+++ routes/quote_rest_api.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const quotes = require('../config/quotes');
 
 // Send a GET request to /quotes to READ a list of quotes
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         res.json(await quotes.getQuotes());
     } catch(err) {
-        res.json({message: err.message});
+        next(err);
     }
 });
 
 // Send a GET request to /quotes/:id to READ a quote
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     try {
         const quote = await quotes.getQuote(req.params.id);
         if (quote) {
@@ -21,13 +21,13 @@ router.get('/:id', async (req, res) => {
             res.status(404).json({message: "Quote not found"});
         }
     } catch(err) {
-        res.status(500).json({message: err.message});
+        next(err);
     }
 });
 
 // Send a GET request to /quotes/random to READ a random quote
 // Send a POST request to /quotes to CREATE a new quote
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         if (req.body.author && req.body.quote) {
             res.status(201).json(await quotes.createQuote({
@@ -38,12 +38,12 @@ router.post('/', async (req, res) => {
             res.status(400).json({message: "Quote and author required"});
         }
     } catch(err) {
-        res.status(500).json({message: err.message});
+        next(err);
     }
 });
 
 // Send a PUT request to /quotes/:id to UPDATE a quote
-router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res, next) => {
     try {
         const quote = await quotes.getQuote(req.params.id);
         if(quote) {
@@ -55,12 +55,12 @@ router.put('/:id', async (req, res) => {
             res.status(404).json({message: "Quote not found"});
         }
     } catch(err) {
-        res.status(500).json({message: err.message});
+        next(err);
     }
 });
 
 // Send a DELETE request to /quotes/:id to DELETE a quote
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
     try {
         const quote = await quotes.getQuote(req.params.id);
         if(quote) {
@@ -70,8 +70,23 @@ router.delete('/:id', async (req, res) => {
             res.status(404).json({message: "Quote not found"});
         }
     } catch(err) {
-        res.status(500).json({message: err.message});
+        next(err);
     }
+});
+
+router.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
 });
 
 module.exports = router;
