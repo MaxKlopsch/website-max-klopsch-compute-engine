340c4dddbf9f0390a838f86f2a43547ce9efbceb
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-03-26 13:45:25 +0000

    Refactor async routes using an async handler to reduce duplicate code

--- routes/quote_rest_api.js
+++ routes/quote_rest_api.js
@@ -2,77 +2,67 @@ const express = require('express');
 const router = express.Router();
 const quotes = require('../config/quotes');
 
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+        try {
+            await cb(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    };
+}
+
 // Send a GET request to /quotes to READ a list of quotes
-router.get('/', async (req, res, next) => {
-    try {
-        res.json(await quotes.getQuotes());
-    } catch(err) {
-        next(err);
-    }
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+    res.json(await quotes.getQuotes());
+}));
 
 // Send a GET request to /quotes/:id to READ a quote
-router.get('/:id', async (req, res, next) => {
-    try {
-        const quote = await quotes.getQuote(req.params.id);
-        if (quote) {
-            res.json(quote);
-        } else {
-            res.status(404).json({message: "Quote not found"});
-        }
-    } catch(err) {
-        next(err);
+router.get('/:id', asyncHandler(async (req, res, next) => {
+    const quote = await quotes.getQuote(req.params.id);
+    if (quote) {
+        res.json(quote);
+    } else {
+        res.status(404).json({message: "Quote not found"});
     }
-});
+}));
 
 // Send a GET request to /quotes/random to READ a random quote
 // Send a POST request to /quotes to CREATE a new quote
-router.post('/', async (req, res, next) => {
-    try {
-        if (req.body.author && req.body.quote) {
-            res.status(201).json(await quotes.createQuote({
-                quote: req.body.quote,
-                author: req.body.author
-            }));
-        } else {
-            res.status(400).json({message: "Quote and author required"});
-        }
-    } catch(err) {
-        next(err);
+router.post('/', asyncHandler(async (req, res, next) => {
+    if (req.body.author && req.body.quote) {
+        res.status(201).json(await quotes.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        }));
+    } else {
+        res.status(400).json({message: "Quote and author required"});
     }
-});
+}));
 
 // Send a PUT request to /quotes/:id to UPDATE a quote
-router.put('/:id', async (req, res, next) => {
-    try {
-        const quote = await quotes.getQuote(req.params.id);
-        if(quote) {
-            quote.quote = req.body.quote;
-            quote.author = req.body.author;
-            await quotes.updateQuote(quote);
-            res.status(204).end();
-        } else {
-            res.status(404).json({message: "Quote not found"});
-        }
-    } catch(err) {
-        next(err);
+router.put('/:id', asyncHandler(async (req, res, next) => {
+    const quote = await quotes.getQuote(req.params.id);
+    if(quote) {
+        quote.quote = req.body.quote;
+        quote.author = req.body.author;
+        await quotes.updateQuote(quote);
+        res.status(204).end();
+    } else {
+        res.status(404).json({message: "Quote not found"});
     }
-});
+}));
 
 // Send a DELETE request to /quotes/:id to DELETE a quote
-router.delete('/:id', async (req, res, next) => {
-    try {
-        const quote = await quotes.getQuote(req.params.id);
-        if(quote) {
-            await quotes.deleteQuote(quote);
-            res.status(204).end();
-        } else {
-            res.status(404).json({message: "Quote not found"});
-        }
-    } catch(err) {
-        next(err);
+router.delete('/:id', asyncHandler(async (req, res, next) => {
+    const quote = await quotes.getQuote(req.params.id);
+    if(quote) {
+        await quotes.deleteQuote(quote);
+        res.status(204).end();
+    } else {
+        res.status(404).json({message: "Quote not found"});
     }
-});
+}));
 
 router.use((req, res, next) => {
     const err = new Error("Not Found");
